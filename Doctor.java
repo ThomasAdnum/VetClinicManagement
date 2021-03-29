4ce62d07492583e83caabd0767f85f3484d54632
ThomasAdnum <31847081+ThomasAdnum@users.noreply.github.com>
2021-03-29 14:43:33 +1100

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,17 @@
+public class Doctor 
+{
+	private String name; 	// the name of the doctor
+	private String specialisation; 	// the specialisation of the doctor (it can be â€œdogâ€ or â€œcatâ€)
+	
+	// add comments
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	// add comments
+	public String getName()
+	{
+		return name;
+	}
+	// complete all other methods from here.
+}
