d049a0a66a2668cf2630b1e6bf31652c6bc725f3
ThomasAdnum <31847081+ThomasAdnum@users.noreply.github.com>
2021-03-29 14:42:45 +1100

    Create Clinic.java

---
+++ Clinic.java
@@ -0,0 +1,12 @@
+public class Clinic 
+{
+	private void run() 
+	{
+	//This method should control the flow of the program 
+	}
+	public static void main(String[] args)
+	{
+    		Clinic clinic = new Clinic();
+    		clinic.run();
+	}
+}
