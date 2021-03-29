af3fa670540ee78da4d8c7c24470e4a5ee8f02e9
ThomasAdnum <31847081+ThomasAdnum@users.noreply.github.com>
2021-03-29 14:44:06 +1100

    Create Pet.java

---
+++ Pet.java
@@ -0,0 +1,21 @@
+public class Pet 
+{
+	private String type; 	// the type of the pet. It can be only â€œcatâ€ or â€œdogâ€.
+	private String size; 	// the size of the pet. It can be only â€œsmallâ€, â€œmediumâ€ or â€œlargeâ€.
+	private String name; 	// the name of the pet. 
+	private double weight; 	//the weight of the pet.
+	private int age; 		//the age of the pet.
+	private String doctor;  // the doctor of the pet.
+	
+	// add comments
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	// add comments
+	public String getType()
+	{
+		return type;
+	}
+	// complete all other methods from here.
+}
